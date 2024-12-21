19230052a88d041336567b56ea04260bb9e784be
Saayemm <sayemdcc@gmail.com>
2024-12-21 21:41:53 +0600

    nested if else

--- basicJs/conditional.js
+++ basicJs/conditional.js
@@ -58,16 +58,50 @@ based on the following grading scale:
     F: 0-59
 
 ***/
-let marks = 58;
+// let marks = 58;
 
-if(marks>=90){
-    console.log("A");
-}else if(marks>=80 && marks <=90){
-    console.log("B");
-}else if(marks>=70  && marks<=80){
-    console.log("C");
-}else if(marks>=60 && marks<= 70){
-    console.log("D");
-}else if(marks<=60){
-    console.log("F");
+// if(marks>=90){
+//     console.log("A");
+// }else if(marks>=80 && marks <=90){
+//     console.log("B");
+// }else if(marks>=70  && marks<=80){
+//     console.log("C");
+// }else if(marks>=60 && marks<= 70){
+//     console.log("D");
+// }else if(marks<=60){
+//     console.log("F");
+// }
+
+/***
+
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else
+*/
+
+let myScore = 88;
+let myFriendScore = 37;
+
+if(myScore>80){
+    if(myFriendScore>80){
+        console.log("Let's go for a lunch");
+    }else if(myFriendScore>60 && myFriendScore<80){
+        console.log("Hey bro! good luck next time");
+        
+    }else if(myFriendScore>=40 && myFriendScore<60){
+        console.log("I can't seen your message bro");
+        
+    }else if(myFriendScore<40){
+        console.log("sorry! for block you");
+        
+    }
+}else if(myScore<80){
+    console.log("I'm so sad! I have to go home and I have to sleep");
+    
 }
