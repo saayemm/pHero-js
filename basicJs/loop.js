803263c02fb02aa46c8ea67b0e09311e87516fb7
Saayemm <sayemdcc@gmail.com>
2024-12-22 00:48:24 +0600

    loop

---
+++ basicJs/loop.js
@@ -0,0 +1,6 @@
+const number = [12, 23, 34, 45, 56, 67, 78, 89, 90, 100, 155, 166, 177, 188, 199, 200];
+for(const num of number){
+    console.log(num);
+    console.log("this is a number");
+    
+}
