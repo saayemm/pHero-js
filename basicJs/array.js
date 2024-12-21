6c6087294e9ef23c587118058ef90a91682bc159
Saayemm <sayemdcc@gmail.com>
2024-12-21 23:32:32 +0600

    array

---
+++ basicJs/array.js
@@ -0,0 +1,16 @@
+const number = [12, 23, 34, 45, 56, 67, 78, 89, 90, 100, 155, 166, 177, 188, 199, 200];
+// console.log(number.length);
+
+// Index
+// console.log(number[5]);
+
+const third = number[3];
+// console.log(third);
+
+number[0] = 1000;
+// console.log(number);
+
+const newNumber = [12, 23, 34, 45, 56, 67, 78, 89, 90, 100, 155, 166, 177, 188, 199, 200];
+console.log(newNumber);
+newNumber.push(250);
+console.log(newNumber);
