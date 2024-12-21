4aa1fe63cea3410557c5c4e32f264237d7d1412f
Saayemm <sayemdcc@gmail.com>
2024-12-21 21:18:55 +0600

    first task

---
+++ basicJs/conditional.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 600;
+
+if(burgerPrice > 500){
+    console.log('Oyee! you have a free coke');
+}else{
+    console.log('you have to buy a coke with 30tk');
+}
