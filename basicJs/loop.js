ccea1923b43f92ceb345c106f78edc7f475b5413
Saayemm <sayemdcc@gmail.com>
2024-12-22 08:49:30 +0600

    find odd

--- basicJs/loop.js
+++ basicJs/loop.js
@@ -1,6 +1,49 @@
-const number = [12, 23, 34, 45, 56, 67, 78, 89, 90, 100, 155, 166, 177, 188, 199, 200];
-for(const num of number){
-    console.log(num);
-    console.log("this is a number");
+// const number = [12, 23, 34, 45, 56, 67, 78, 89, 90, 100, 155, 166, 177, 188, 199, 200];
+// for(const num of number){
+//     console.log(num);
+//     console.log("this is a number");
     
+// }
+
+// const num = 7;
+// if(num<5){
+//     console.log("choto number");
+    
+// }else{
+//     console.log("eber thik ase");
+    
+// }
+
+// let num1 = 0;
+// let sum = 0;
+
+// while(num1<=10){
+//     // console.log(num1);
+//     num1++
+//     sum = sum+num1;
+//     console.log(sum);
+    
+    
+// }
+
+/***
+
+Subtask-1:
+Find all the odd numbers from 61 to 100.
+ */
+
+/***
+Subtask-2:
+Find all the even numbers from 78 to 98.
+
+ */
+
+let num = 61;
+
+while(num<=100){
+    if(num%2===1){
+        console.log("odd n", num);
+        
+    }
+   num++
 }
