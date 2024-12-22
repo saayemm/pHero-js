f4ef7ac03446a74d7d11f8e4b3a22b1418309055
Saayemm <sayemdcc@gmail.com>
2024-12-22 10:32:34 +0600

    string method

---
+++ string/string.js
@@ -0,0 +1,25 @@
+const capital = "Dhaka";
+// console.log(capital.length);
+// console.log(capital.indexOf("a"));
+// console.log(capital.lastIndexOf("a"));
+// console.log(capital.charAt(0));
+// console.log(capital[3]);
+
+const country = "Bangladesh";
+const upper = country.toUpperCase();
+// console.log(upper);
+
+const division = "Dhaka";
+const part = division.slice(1,3)
+// console.log(part);
+
+
+const country1 = "Bangladesh";
+// console.log(country1.split(""));
+
+
+const fullAddress = division.concat(" ", country1);
+// console.log(fullAddress);
+
+const sentence = "I love my country";
+
