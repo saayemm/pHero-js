873f110419ede73cb429fe4446bfbc28b2ce81dd
Saayemm <sayemdcc@gmail.com>
2024-12-22 19:35:33 +0600

    math

---
+++ basicJs/math.js
@@ -0,0 +1,15 @@
+const min = Math.min(1, 2, 3, 4, 5); // 1
+console.log(min);
+console.log(Math.max(1, 2, 3, 4, 5)); // 5
+console.log(Math.round(1.5)); // 2
+console.log(Math.floor(1.5)); // 1
+console.log(Math.ceil(1.5)); // 2
+console.log(Math.random()); // 0.123456789
+console.log(Math.random() * 10); // 1.23456789
+console.log(Math.floor(Math.random() * 10)); // 1
+console.log(Math.floor(Math.random() * 10)); // 5
+
+
+
+
+
