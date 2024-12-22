e1fa34d6bde4600fb4c00d2b61ebde6c620b0635
Saayemm <sayemdcc@gmail.com>
2024-12-22 10:38:44 +0600

    object basic

---
+++ string/object.js
@@ -0,0 +1,11 @@
+const bottle = {
+    color: 'yellow',
+    hold: 'water',
+    price: 50,
+    isCleaned: true,
+    varient: {
+        name: 'cool',
+        made: 'china'
+    }
+}
+console.log(bottle);
