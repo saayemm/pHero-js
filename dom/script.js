0d0b8a9b1db2797f38663653c548b8c9169ca163
Saayemm <sayemdcc@gmail.com>
2024-12-23 21:33:59 +0600

    delegate

--- dom/script.js
+++ dom/script.js
@@ -1,3 +1,18 @@
-function clickRed(){
-    document.body.style.backgroundColor = "red";
-}
+// document.getElementById("item-2").addEventListener("click", function() {
+//   console.log("You clicked on item 2");
+// });
+
+// document.getElementById("list").addEventListener("click", function() {
+//   console.log("You clicked on the list");
+// });
+
+// document.getElementById("item-section").addEventListener("click", function() {
+//   console.log("You clicked on the container");
+// });
+
+const items = document.querySelectorAll(".item");
+  for(const item of items) {
+    item.addEventListener("click", function(e) {
+     e.target.parentNode.removeChild(e.target);
+    });
+  }
